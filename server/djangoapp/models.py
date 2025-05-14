6a85d0272369e3e09508fa59ee97ebbe400863f0
shahijap <p.shahija786@gmail.com>
2025-05-14 11:12:00 -0400

    added dynamic pages

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,7 +19,7 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon')
-    ],
+    ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV'),
     year = models.IntegerField(default=2023,
         validators=[
