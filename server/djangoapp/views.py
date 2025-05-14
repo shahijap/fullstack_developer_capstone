e61441aa2321d7fd515e55818e6c62a269e0756e
shahijap <p.shahija786@gmail.com>
2025-05-14 02:27:06 -0400

    added dynamic pages for dealers and reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 from .models import CarMake, CarModel
 
 
